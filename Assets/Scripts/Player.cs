8c0ab8346a127008026e709c45f0985feea8f8d9
Supetorus <66396555+Supetorus@users.noreply.github.com>
2022-04-28 10:15:26 -0600

    bunches of stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CharacterController2D characterController;
+    [SerializeField] Animator animator;
+    [SerializeField] float speed;
+
+    Vector2 direction = Vector2.zero;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+		direction.x = Input.GetAxis("Horizontal") * speed;
+        animator.SetFloat("Speed", Mathf.Abs(direction.x));
+
+        if (Input.GetButtonDown("Jump"))
+		{
+            animator.SetBool("Jumping", true);
+            jump = true;
+		}
+
+        characterController.Move(direction.x, false, jump);
+    }
+
+	private void FixedUpdate()
+	{
+        characterController.Move(direction.x * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+	}
+
+    public void OnLand()
+	{
+        animator.SetBool("Jumping", false);
+    }
+}
